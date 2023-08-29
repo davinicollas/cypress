e735a7817e7c73424a02fbcea56246dfff5eef70
davinicollas <davi.nicollas@hotmail.com>
2023-08-29 09:04:08 -0300

    Teste na tela de cadastro / login

---
+++ cypress/e2e/cadastro-correto.cy.js
@@ -0,0 +1,13 @@
+describe('Pagina de cadastro', () => {
+  it('preencher os campos do formulario corretamente para cadastrar um novo usuario', () => {
+    cy.visit('http://localhost:4200/#/home')
+    cy.contains('a','Register now').click();
+    cy.get('[data-test="email"]').type('davi@hotmail.com');
+    cy.get('[data-test="fullName"]').type('davi nicollas');
+    cy.get('[data-test="registerUserName"]').type('davinicollas');
+    cy.get('[data-test="registerPassword"]').type('12345678');
+    cy.contains('button','Register').click();
+
+
+  })
+})
