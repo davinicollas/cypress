2ad6dd510c2710b77992bb32e3c66c237f1fb6ba
davinicollas <davi.nicollas@hotmail.com>
2023-08-29 10:07:02 -0300

    criação de tes de api

--- cypress/e2e/login-incorreto.cy.js
+++ cypress/e2e/login-incorreto.cy.js
@@ -1,7 +1,22 @@
-describe('Pagina de login', () => {
-    it('Verificar mensagens vampos obrigatorios', () => {
-      cy.visit('http://localhost:4200/#/home')
-      cy.contains('User name is required!').should('be.visible');
-      cy.contains('Password is required!').should('be.visible');
+describe('Página de login', () => {
+
+  beforeEach(() => {
+      cy.visit ('http://localhost:4200/#/home');
+      cy.intercept('POST', 'http://localhost:3000/user/login', {
+        statusCode: 400
+        }).as('stubPost');
     })
-  })
+
+          
+      it('Verifica mensagens de campos obrigatórios na página de login', () => {
+        cy.contains('User name is required!').should('be.visible');
+        cy.contains('Password is required!').should('be.visible');
+      });
+
+      it('Preencher os campos de login corretamente para realizar login', () => {
+        cy.login('davinicollas', '12345678');
+        cy.wait('@stubPost')
+
+      });
+
+})
