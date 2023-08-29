4b223b542111e2dffc6e197b3b6b6e6e22495944
davinicollas <davi.nicollas@hotmail.com>
2023-08-29 10:25:57 -0300

    Requisição teste

--- cypress/e2e/api-alurapic.cy.js
+++ cypress/e2e/api-alurapic.cy.js
@@ -1,5 +1,5 @@
 describe('Api Alurapic', () => {
-  it('Dados APi', () => {    
+ /* it('Dados APi', () => {    
     cy.request({
       method: 'POST',
       url: 'http://localhost:3000/user/login',
@@ -9,6 +9,18 @@ describe('Api Alurapic', () => {
       expect(res.body).is.not.empty
       expect(res.body).to.have.property('id')
       expect(res.body.id).to.be.equal(3)
+    })
+  })*/
+
+  it('Fotos usuarios', () => {    
+    cy.request({
+      method: 'GET',
+      url: 'http://localhost:3000/davinicollas/photos'
+    }).then((res)=> {
+      expect(res.status).to.be.equal(200)
+      expect(res.body).is.not.empty
+      expect(res.body[0]).to.have.property('description')
+      expect(res.body[0].description).to.be.equal('')
     })
   })
 })
